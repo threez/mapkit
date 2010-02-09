0d2868e56db2295b8afddd17e7cbeca6c0f4ea7d
Vincent Landgraf <vincentlandgraf@Vincent-MacBook.local>
2010-02-09 12:55:53 +0100

    added code to remove duplicates

--- crawl.rb
+++ crawl.rb
@@ -7,3 +7,7 @@ DB = Sequel.postgres "varta", :user => "varta", :passwd => "varta"
 puts "start crawling..."
 Google.crawl_region_to_db(DB[:points_of_interest], "Tankstelle", [48.87, 8.57], 0.005, 4)
 puts "crawling finished"
+puts "remove duplicates..."
+DB.run "update points_of_interest set flag = 1 where id in (select min(id) FROM points_of_interest group by lat, lng)"
+DB.run "delete from points_of_interest where flag is null"
+puts "removed duplicates"
